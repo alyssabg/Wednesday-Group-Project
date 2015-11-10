6bedbff58dc6be8df7acfa8a2cda4a964edb4542
Logan La Vigne <lavigne.logan@knights.ucf.edu>
2015-11-10 13:32:22 -0500

    Users DB
    Users DB

--- users.sql
+++ users.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:8889
--- Generation Time: Oct 20, 2015 at 07:48 PM
--- Server version: 5.5.42
--- PHP Version: 5.6.10
+-- Host: sulley.cah.ucf.edu
+-- Generation Time: Nov 10, 2015 at 12:28 PM
+-- Server version: 5.5.46-0ubuntu0.14.04.2
+-- PHP Version: 5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `lo071617`
@@ -54,15 +60,18 @@ CREATE TABLE `users` (
   `UserCountryShip` varchar(20) COLLATE latin1_german2_ci NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `UserStatus` int(11) DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`UserID`, `UserEmail`, `UserPassword`, `UserFirstName`, `UserLastName`, `UserCity`, `UserState`, `UserZip`, `UserRegistrationDate`, `UserPhone`, `UserCountry`, `UserAddress`, `UserAddress2`, `UserFirstBill`, `UserLastBill`, `UserCreditCard`, `UserCCV`, `UserAddressBill`, `UserAddress2Bill`, `UserCityBill`, `UserStateBill`, `UserZipBill`, `UserCountryBill`, `UserFirstNameShip`, `UserLastNameShip`, `UserAddressShip`, `UserAddress2Ship`, `UserCityShip`, `UserStateShip`, `UserZipShip`, `UserCountryShip`, `timestamp`, `UserStatus`) VALUES
-(1, 'wednesdayadmin@wednesday.com', '209236a2b31080a8a80ecb8bbf427c5ea476e196', 'Wednesday', 'Admin', 'Orlando', 'FL', '32816', '2015-10-13 17:53:17', '3866828625 ', 'United States', '4000 Central Florida Boulevard', '', '', '', 0, 0, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00 00:00:00', 3),
-(2, 'lavigne.logan@hotmail.com', '05057a9cffb14dab4c45a6815933431335aaeb01', 'Logan', 'La Vigne', 'West Melbourne', 'FL', '32904', '2015-10-13 17:56:07', '8287838129', 'United States', '825 Greenwood Manor Circle', '', 'Logan', 'La Vigne', 1234567812345678, 123, '825 Greenwood Manor Circle', '', 'West Melbourne', 'FL', '32904', 'United States', 'Logan', 'La Vigne', '825 Greenwood Manor Circle', '', 'West Melbourne', 'FL', '32904', 'United States', '0000-00-00 00:00:00', 2);
+(1, 'Admin', '1209227dd65df46bbbf90bd4d1e97fa1de21ded8', 'Wednesday', 'Admin', 'Orlando', 'FL', '32816', '2015-10-13 17:53:17', '3866828625 ', 'United States', '4000 Central Florida Boulevard', '', '', '', 0, 0, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00 00:00:00', 3),
+(2, 'lavigne.logan@hotmail.com', '05057a9cffb14dab4c45a6815933431335aaeb01', 'Logan', 'La Vigne', 'West Melbourne', 'FL', '32904', '2015-10-13 17:56:07', '8287838129', 'United States', '825 Greenwood Manor Circle', '', 'Logan', 'La Vigne', 1234567812345678, 123, '825 Greenwood Manor Circle', '', 'West Melbourne', 'FL', '32904', 'United States', 'Logan', 'La Vigne', '825 Greenwood Manor Circle', '', 'West Melbourne', 'FL', '32904', 'United States', '2015-10-27 21:57:17', 2),
+(12, 'Super', 'a82f4eadda2b8a04d92bdd21350b95561ae3d93e', 'Dan', 'Novatnak', 'Orlando', 'FL', '28604', '2015-11-04 22:42:58', '407-823-2000', 'United States', '4000 Central Florida Boulevard', '', '', '', 0, 0, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00 00:00:00', 1),
+(13, 'hereisaemail@gmail.com', 'b0bea722005cba0656b64dccf2cee89a972d8d90', 'Alyssa', 'Gagnon', 'Orlando', 'FL', '32817', '2015-11-10 03:01:27', '4071231234', 'United States', '123 Over There dr', '', '', '', 0, 0, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00 00:00:00', 1),
+(14, 'mary@gmail.com', '3f518922c77a035d8e50abcec8c57d6a9003922f', 'Mary', 'Forgottheemail', 'Orlando', 'CT', '34562', '2015-11-10 03:04:08', '1234567', 'United States', 'Mary Over Here dr', '', '', '', 0, 0, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00 00:00:00', 1);
 
 --
 -- Indexes for dumped tables
@@ -82,4 +91,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
