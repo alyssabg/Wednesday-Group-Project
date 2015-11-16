3837fecd0fe14a69c0a005990dc9a72900c82710
alyssabg <alyssa.gagnon93@gmail.com>
2015-11-15 19:47:57 -0500

    mobile update

--- login.php
+++ login.php
@@ -100,12 +100,13 @@
         </div>
     </section>
 
-            <div class="tab-content" style="margin-top: -100px;">
+            <div class="tab-content" style="margin-top: -100px; padding-top:60px;">
                 <div id="menu1" class="tab-pane fade in active">
                     <section class="container" style="padding-top: 15px; margin-bottom: 13%;">
                          <div class="container-page">                
                             <div class="col-md-6">
-                                <h3 class="dark-grey"><span class="wed_color">Log In</span></h3>
+                              <h3 class="dark-grey"><span class="wed_color">Log In</span></h3>
+                               
                                 
                                 <form method="post" action="php/login_check.php">
                                 <fieldset class="clearfix">
