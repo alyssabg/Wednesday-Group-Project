fc57abbb9aa410a3b2e3695606e9e3bc2b1fd56f
StephaniQ <stephy2012@knights.ucf.edu>
2015-11-17 12:05:33 -0500

    Update cart.php

--- cart.php
+++ cart.php
@@ -196,12 +196,12 @@ if(isset($_SESSION["cart_item"])){
           <div class="row">
                     	<div class="col-sm-12 col-md-12 pull-right">
                       
-                   <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                        <input type="hidden" name="cmd" value="_s-xclick">
-                        <input type="hidden" name="hosted_button_id" value="ENWUPEB8FTUDS">
-                        <input type="submit" class="btn btn-success pull-right" value="Checkout" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                        <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-                    </form>
+                  <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            		<input type="hidden" name="cmd" value="_s-xclick">
+            		<input type="hidden" name="hosted_button_id" value="VBPJY5U4H3CKL">
+            		<input type="submit" class="btn btn-success pull-right" value="Checkout" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            		<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        	</form>
                 
 
                 
